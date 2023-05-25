f6a922a4031e1c717e65b5e9d61e3463ccc9feee
Geoffrey Otieno <otienogeoffrey812@gmai.com>
2023-05-25 21:50:12 +0300

    Most Repeated Character in  a string

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         StringUtils stringUtils = new StringUtils();
-
         System.out.println(stringUtils.mostRepeatedCharacter("Hellloooo!!"));
     }
 }
