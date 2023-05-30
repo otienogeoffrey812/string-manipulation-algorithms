5a071e8d4d374c435b0a7c876b64c54dc0d43257
Geoffrey Otieno <otienogeoffrey812@gmai.com>
2023-05-30 23:35:24 +0300

    Count Vowels In a String

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         StringUtils stringUtils = new StringUtils();
 
-        System.out.println(stringUtils.reverseString("Test 1234"));
+        System.out.println(stringUtils.countVowels("Test 12iie34"));
     }
 }
