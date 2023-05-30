e97c1906925d1705f17be1ce0aac0682222f0e4d
Geoffrey Otieno <otienogeoffrey812@gmail.com>
2023-05-30 23:45:03 +0300

    Capitalize Words And Remove Extra Spaces

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         StringUtils stringUtils = new StringUtils();
-        System.out.println(stringUtils.mostRepeatedCharacter("Hellloooo!!"));
+        System.out.println(stringUtils.capitalizeAndRemoveExtraSpaces("  trees    are        beautiful  "));
     }
 }
